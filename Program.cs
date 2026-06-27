2d16d62a0184a3b52c6d3ff5e667340391e8a8c0
binaykushwaha <iambinaykushwaha@gmail.com>
2026-06-27 22:25:49 +0545

    Add project structure. Add login page

--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartDanfe.WebApp;
+using SmartDanfe.WebApp.Models;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor();
+SyncfusionLicenseProvider.RegisterLicense(
+    "Ngo9BigBOggjGyl/VkV+XU9AclRDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS3hTcEdlWXdeeXFTRmVeVU91XA==");
+builder.Services.AddScoped<LoginModel>();
 
 await builder.Build().RunAsync();
